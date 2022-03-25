7a9f38e8d3878342db8907a80d826484f117ce48
Greg Lyons <gregory.lyons512@gmail.com>
2022-03-25 18:14:48 -0500

    Netlify test deploy 8.

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,11 @@ import reportWebVitals from './reportWebVitals';
 const httpLink = createHttpLink({
   // URL for API
   uri: process.env.REACT_APP_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
 });
 
 const authLink = setContext((_, { headers }) => {
